21ba7101940e28a2b7af3b30fd52cedd4d57eca8
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-06-26 12:38:11 -0600

    added media queries

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,6 @@ import React from "react"
 export default function Main() {
     return (
         <main className="main--card">
-            <img src="../assets/React_logo.png" alt="" />
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
